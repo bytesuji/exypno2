0dcd1627246917bd2eca298e6248de23997b1697
Albert Brox <albert@exypno.tech>
2019-11-13 21:17:47 -0500

    color change JS, portfolio windows

--- js/animations.js
+++ js/animations.js
@@ -1,10 +1,4 @@
 // Auto-size portrait
-var logoWidth = document.getElementsByTagName('h1')[0].offsetWidth;
-var portrait = document.getElementsByClassName('portrait')[0];
-var portraitWidth = logoWidth * 0.61804 // inverse golden ratio
-portrait.setAttribute('style', 'width:' + portraitWidth + 'px');
-
-// expand about text
 $("#about").click(function() {
   if ($("#line2").css('margin-bottom') == 0) {
     $("#line2").css('margin-bottom', "8%");
@@ -12,5 +6,31 @@ $("#about").click(function() {
     $("#line2").css('margin-bottom', 0);
   }
 
-  $(".about").slideToggle("slow");
-})
+  $("#about-text").slideToggle("fast");
+  window.scrollBy(0, 290);
+});
+
+$("#portfolio").click(function() {
+  $(".portfolio").slideToggle("fast");
+  window.scrollBy(0, 400);
+});
+
+// function randomInt(min, max) {
+//   return Math.floor(Math.random() * Math.floor(max) + min);
+// }
+
+// $("html, body").click(function() {
+//   var hueShift = randomInt(40, 180);
+//
+//   var newBG = chroma($("body").css('background-color')).set('hsl.h', hueShift);
+//   var newBorder = chroma("#8aafc4").set('hsl.h', hueShift);
+//   var textShadowColor =  "-3px 0 " + newBorder + ", 0 3px " + newBorder +
+//    ", 3px 0 " + newBorder + ", 0 -3px " + newBorder;
+//
+//   $("h1").css('text-shadow', textShadowColor);
+//   $("body").css('background-color', newBG);
+//   $('.portrait').css('border', '3px solid '+newBorder);
+//   $('.fakelink, a').hover(function() {
+//     $(this).css('color', newBorder);
+//   })
+// });
